6957562e47fc80dcc605b27afac6cff3aa92b606
Nathan <nathan.patel@lincoln.ox.ac.uk>
2026-03-02 00:49:39 +0000

    Q3

--- part-1/stack_enhanced.hpp
+++ part-1/stack_enhanced.hpp
@@ -12,14 +12,17 @@ template <typename T> class StackEnhanced : public Stack<T>
 
     void clear()
     {
-        // WRITE YOUR CODE HERE
-    }
+    // Setting _head to 0 makes the stack "empty" instantly.
+    // We do not need to erase _storage; the stack just forgets old values.
+    this->_head = 0;    }
 };
 
+// Allow: stack << value1 << value2
 template <typename T>
 StackEnhanced<T> &operator<<(StackEnhanced<T> &stack, const T &value)
 {
-    // WRITE YOUR CODE HERE
+  stack.push(value);   // push one element on top
+  return stack;        // return the same stack so chaining works
 }
 
 #endif // __stack_enhanced___
