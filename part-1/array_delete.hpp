c665dbc2a5be74188b2b28a501139f5409cda3e5
Nathan <nathan.patel@lincoln.ox.ac.uk>
2026-03-02 00:41:13 +0000

    Q2

--- part-1/array_delete.hpp
+++ part-1/array_delete.hpp
@@ -8,7 +8,28 @@
 template <typename T>
 void array_delete(std::vector<T> &A, std::size_t index)
 {
-    // WRITE YOUR CODE HERE
-}
+void array_delete(std::vector<T> &A, std::size_t index)
+{
+  // 1) Safety check:
+  if (index >= A.size()) {
+    return;
+  }
+
+  // 2) Shift items LEFT to fill the gap.
+  //
+  // We start at the deletion point.
+  // Each step copies/moves the item from the right into the current spot.
+  //
+  // Stop when j is the second-to-last index,
+  // because we read A[j+1].
+  for (std::size_t j = index; j + 1 < A.size(); ++j) {
+    A[j] = std::move(A[j + 1]);
+  }
+
+  // 3) Remove the last element.
+  // After shifting, the final element is duplicated,
+  // so we shrink the vector by 1.
+  A.pop_back();
+}}
 
 #endif // __array_delete__
